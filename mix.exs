1a978e5d714024e76d6b8275ac1c5a0e16d8f379
Miguel Palhas <mpalhas@gmail.com>
2025-04-09 16:05:56 +0100

    wip

--- mix.exs
+++ mix.exs
@@ -13,9 +13,6 @@ defmodule Ethui.MixProject do
     ]
   end
 
-  # Configuration for the OTP application.
-  #
-  # Type `mix help compile.app` for more information.
   def application do
     [
       mod: {Ethui.Application, []},
@@ -23,13 +20,9 @@ defmodule Ethui.MixProject do
     ]
   end
 
-  # Specifies which paths to compile per environment.
   defp elixirc_paths(:test), do: ["lib", "test/support"]
   defp elixirc_paths(_), do: ["lib"]
 
-  # Specifies your project dependencies.
-  #
-  # Type `mix help deps` for examples and options.
   defp deps do
     [
       {:phoenix, "~> 1.7.21"},
@@ -45,16 +38,11 @@ defmodule Ethui.MixProject do
       {:muontrap, "~> 1.6"},
       {:tesla, "~> 1.4", only: :test},
       {:mix_test_watch, "~> 1.0", only: [:dev, :test], runtime: false},
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false}
       {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false}
     ]
   end
 
-  # Aliases are shortcuts or tasks specific to the current project.
-  # For example, to install project dependencies and perform other setup tasks, run:
-  #
-  #     $ mix setup
-  #
-  # See the documentation for `Mix` for more info on aliases.
   defp aliases do
     [
       setup: ["deps.get"]
