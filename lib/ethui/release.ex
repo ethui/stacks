6f32aa2b3538bb00ba44e7b60933eb1b2a58be7c
Miguel Palhas <mpalhas@gmail.com>
2025-05-14 17:21:45 +0100

    wip

---
+++ lib/ethui/release.ex
@@ -0,0 +1,18 @@
+defmodule Ethui.Release do
+  @app :ethui
+
+  def migrate do
+    for repo <- repos() do
+      Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :up, all: true))
+    end
+  end
+
+  def rollback(repo, version) do
+    {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :down, to: version))
+  end
+
+  defp repos do
+    Application.load(@app)
+    Application.fetch_env!(@app, :ecto_repos)
+  end
+end
