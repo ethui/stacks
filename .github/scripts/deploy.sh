a30bb99afc888184f9c36b1c12df20f5cc4cb6a3
Miguel Palhas <mpalhas@gmail.com>
2025-05-15 22:30:58 +0100

    wip

---
+++ .github/scripts/deploy.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+set -e
+
+root=$HOME/stacks.ethui.dev
+release_path=$root/build
+new_release_path=$root/build-new
+bin=$root/build/bin/ethui
+
+cd $root
+
+if [[ ! -f env.sh ]]; then
+    echo "$root/env.sh not found"
+    exit 1
+fi
+
+if [[ ! -f $new_release_path ]]; then
+    echo "$new_release_path not found"
+    exit 1
+fi
+
+source env.sh
+
+$bin eval "Ethui.Release.before_release"
+$bin stop || true
+
+rm -rf $release_path
+mv $new_release_path $release_path
+
+echo "starting daemon"
+$bin daemon
