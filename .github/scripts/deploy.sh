51bd1318f6ca04681ffdbbfdf0ef1d9388968416
Miguel Palhas <mpalhas@gmail.com>
2025-05-15 22:53:42 +0100

    wip

--- .github/scripts/deploy.sh
+++ .github/scripts/deploy.sh
@@ -9,6 +9,8 @@ bin=$root/build/bin/ethui
 
 cd $root
 
+mkdir -p $root/logs
+
 if [[ ! -f env.sh ]]; then
     echo "$root/env.sh not found"
     exit 1
@@ -23,16 +25,22 @@ source env.sh
 
 $bin eval "Ethui.Release.before_release"
 set +e
-$bin pid
+pid=$($bin pid)
 if [[ $? -ne 0 ]]; then
     echo "ethui not running. not need to stop"
 else
+    echo "old pid: $pid"
     $bin stop
 fi
 set -e
+
+# preserve old logs
+mv $release_path/tmp/log $root/logs/$(date +%Y-%m-%d-%H-%M-%S)
 
 rm -rf $release_path
 mv $new_release_path $release_path
 
 echo "starting daemon"
 $bin daemon
+pid=$($bin pid)
+echo "new pid: $pid"
