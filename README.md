ded64ff53b14f21f2065284266f0331979952650
Miguel Palhas <mpalhas@gmail.com>
2025-05-21 10:26:38 +0100

    wip

--- README.md
+++ README.md
@@ -4,3 +4,11 @@
 > This is an experimental repo, not yet production ready.
 
 An API for managing multiple web3 stacks (anvil, subgraph, etc) for internal and external testing.
+
+## Running locally
+
+```bash
+docker build -t ethui-stacks .
+
+docker run -v $HOME/.config/ethui/stacks/local:$HOME/.config/ethui/stacks/local -e DATA_ROOT:$HOME/.config/ethui/stacks/local -v /var/run/docker.sock:/var/run/docker.sock --init -p 4000:4000 --name=ethui-stacks ethui-stacks
+```
--- rel/overlays/bin/docker-entrypoint.sh
+++ rel/overlays/bin/docker-entrypoint.sh
@@ -1,4 +1,26 @@
 #!/bin/sh
 
+set -eu
+
+if [ -z "${DATA_ROOT:-}" ]; then
+  echo "DATA_ROOT not defined. set it to $HOME/.config/ethui/stacks, here $HOME is your docker host username"
+  exit 1
+fi
+
+if [ -z "${SECRET_KEY_BASE:-}" ]; then
+  if [ -f $DATA_ROOT/phx.secret.key ]; then
+    echo "phx.secret.key detected"
+  else
+    echo "generating a new phx.secret.key"
+    openssl rand -hex 64 >> $DATA_ROOT/phx.secret.key
+  fi
+
+  export SECRET_KEY_BASE="$(cat $DATA_ROOT/phx.secret.key)"
+fi
+
+if [ -z "${PHX_HOST:-}" ]; then
+  export PHX_HOST="local.ethui.dev"
+fi
+
 /app/bin/migrate
 /app/bin/server
