80990f5666c1c02a11d7b2291bcdef81f700a79c
joaocosta9 <johnydacosta@hotmail.com>
2025-12-23 11:51:05 +0000

    add nitro

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,17 @@
 import tailwindcss from "@tailwindcss/vite";
+import { nitroV2Plugin } from "@tanstack/nitro-v2-vite-plugin";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import tsConfigPaths from "vite-tsconfig-paths";
+
+const target = process.env.VITE_TARGET;
+
+const nitroConfig: Record<string, any> = {};
+
+if (target !== "docker") {
+  nitroConfig.preset = "vercel";
+}
 
 export default defineConfig({
   server: {
