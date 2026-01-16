c195343b1416de1165b363548f91e3f86c767059
ZePedroResende <resende@subvisual.co>
2026-01-16 18:46:34 +0000

    refactor

--- server/lib/ethui/stacks.ex
+++ server/lib/ethui/stacks.ex
@@ -115,11 +115,7 @@ defmodule Ethui.Stacks do
     |> Repo.preload(:api_key)
   end
 
-  def list_stacks() do
-    list_stacks(nil)
-  end
-
-  def list_stacks(user) do
+  def list_stacks(user \\ nil) do
     if user do
       Repo.all(from(s in Stack, where: s.user_id == ^user.id))
       |> Repo.preload(:api_key)
