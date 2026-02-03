3170e80779758396f0bc177c9b34fb48e14048a9
Miguel Palhas <mpalhas@gmail.com>
2026-02-03 11:02:40 +0000

    wip

--- server/lib/ethui/telemetry.ex
+++ server/lib/ethui/telemetry.ex
@@ -1,20 +1,8 @@
 defmodule Ethui.Telemetry do
   @moduledoc """
-  Helper module for executing telemetry events with the [:ethui] prefix.
+  Helper module for executing telemetry events with the [:ethui_stacks] prefix.
   """
 
-  @doc """
-  Execute a telemetry event with the [:ethui] prefix automatically added.
-  Measurements are automatically set to %{count: 1}.
-
-  ## Examples
-
-      Ethui.Telemetry.exec([:stacks, :created], %{stack_slug: "demo"})
-      # Executes: :telemetry.execute([:ethui, :stacks, :created], %{count: 1}, %{stack_slug: "demo"})
-
-      Ethui.Telemetry.exec([:auth, :code_sent], %{email: "user@example.com"})
-      # Executes: :telemetry.execute([:ethui, :auth, :code_sent], %{count: 1}, %{email: "user@example.com"})
-  """
   @spec exec(event :: [atom()], metadata :: map()) :: :ok
   def exec(event, metadata \\ %{}) when is_list(event) do
     :telemetry.execute([:ethui_stacks | event], %{count: 1}, metadata)
